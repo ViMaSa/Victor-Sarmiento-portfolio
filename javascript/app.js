47296039ab5abf123f8539fe7e050a2a6babdc8b
Victor <sarmiv07@gmail.com>
2022-02-28 21:51:32 -0800

    Add Footer and stylized landing page

--- javascript/app.js
+++ javascript/app.js
@@ -3,12 +3,19 @@ const animateIcon = () => {
 }
 
 const animateMenu = () => {
-  const $menu = $('#menu-list-container')
+  const $menu = $('#menu-list-container');
+  const $menuContainer = $('#menu-aside')
+  const $nav = $('nav');
+
   if($menu.hasClass('hide')) {
     $menu.addClass('show').removeClass('hide');
+    $menuContainer.addClass('show').removeClass('hide');
+    $nav.addClass('show').removeClass('hide');
   }
   else {
     $menu.addClass('hide').removeClass('show');
+    $menuContainer.addClass('hide').removeClass('show');
+    $nav.removeClass('show');
   }
 }
 
