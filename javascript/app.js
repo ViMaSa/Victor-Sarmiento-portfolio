20cc97b77779767f1dbd0169f82ef406bb2a484d
Victor <sarmiv07@gmail.com>
2022-03-01 21:12:13 -0800

    Resume Page: Completed

--- javascript/app.js
+++ javascript/app.js
@@ -19,5 +19,29 @@ const animateMenu = () => {
   }
 }
 
+const submitInput = () => {
+  const contactInput = {};
+  const $firstName = $('#first-name-input');
+  const $lastName = $('#last-name-input');
+  const $email = $('#email-input');
+  const $subject = $('#inquiry-input');
+
+  // Save values to obj to return if we had a backend
+  contactInput.firstName = $firstName.val();
+  contactInput.lastName = $lastName.val();
+  contactInput.email = $email.val();
+  contactInput.subject = $subject.val();
+
+  // Instead just use dummy alert user of email being sent
+  alert(`Thank you ${contactInput.firstName} ${contactInput.lastName}. Your email about ${contactInput.subject} has been sent!`);
+
+  // Clear values
+  $firstName.val('');
+  $lastName.val('');
+  $email.val('');
+  $subject.val('');
+}
+
 $('.menu-icon').on("click", animateIcon);
 $('.menu-icon').on("click", animateMenu);
+$('#form-submit-button').on("click", submitInput);
